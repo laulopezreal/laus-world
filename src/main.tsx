2130d1a3f6f860c2ceee280874d9e88c7fe62ed8
laulopezreal <lauureal@gmail.com>
2026-01-30 14:57:51 +0100

    fix: layout blocking, default collapse, and gh-pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/laus-world">
       <ThemeProvider>
         <App />
       </ThemeProvider>
